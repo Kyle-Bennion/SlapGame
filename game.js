43847e7dd11545031fdfead9b2c89ae5bb716b7a
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-08-20 16:12:29 -0600

    josh's additions

--- game.js
+++ game.js
@@ -1,84 +1,108 @@
-var health = 100
-var targetName = 'Ben'
-var hits = 0
+var target = {
+  health: 100,
+  targetName: "Ben",
+  hits: 0,
+  items: [],
+};
 
+var items = {
+  fire: { name: "Fire", modifier: 2, description: "IT BURNS!" },
+  hounds: {
+    name: "Hounds",
+    modifier: 10,
+    description: "Who let the dogs out?",
+  },
+  spear: { name: "Spear", modifier: 5, description: "OUCH!" },
+};
 
-let toons = [
-  {
-    health: 140,
-    targetName: "Attilla",
-    hits: 0,
-    img: 'Attilla.jpg'
-  },
-  {
-    health: 130,
-    targetName: "Genghis Khan",
-    hits: 0,
-    img: 'GenghisKhan.PNG'
-  },
-  {
-    health: 100,
-    targetName: "Vlad the Impaler",
-    hits: 0,
-    img: 'Vlad.jpeg'
+function giveItems(itemName) {
+  target.items.push(items[itemName]);
+  console.log(target);
+  addMods();
+}
+
+function addMods() {
+  let runningModTotal = 0;
+
+  for (let i = 0; i < target.items.length; i++) {
+    runningModTotal += target.items[i].modifier;
   }
-]
-
-
-
+  return runningModTotal;
+}
 
 function hitCount() {
-  hits++
-
+  target.hits++;
 }
 
 function displayHits(hits) {
-  let elem = document.getElementById('hits')
-  elem.innerHTML = hits.toString()
+  let elem = document.getElementById("hits");
+  elem.innerHTML = hits.toString();
 }
 
 function slap() {
-  health--
-  hitCount()
-  zeroCheck()
-  healthDisplay(health)
-  displayHits(hits)
-
+  target.health -= 1 + addMods();
+  hitCount();
+  zeroCheck();
+  healthDisplay(target.health);
+  displayHits(target.hits);
 }
 
 function healthDisplay(health) {
-  let elem = document.getElementById('health')
+  let elem = document.getElementById("health");
   if (health == 0) {
-    health = 'Dead'
-    elem.innerHTML = health
-  }
-  else {
-    elem.innerHTML = health.toString()
+    health = "Dead";
+    elem.innerHTML = health;
+  } else {
+    elem.innerHTML = health.toString();
   }
 }
 
 function punch() {
-  health -= 5
+  target.health -= 5 + addMods();
 
-  hitCount()
-  zeroCheck()
+  hitCount();
+  zeroCheck();
 
-  healthDisplay(health)
-  displayHits(hits)
+  healthDisplay(target.health);
+  displayHits(target.hits);
 }
 
 function kick() {
-  health -= 10
+  target.health -= 10 + addMods();
 
-  hitCount()
-  zeroCheck()
+  hitCount();
+  zeroCheck();
 
-  healthDisplay(health)
-  displayHits(hits)
+  healthDisplay(target.health);
+  displayHits(target.hits);
 }
 
 function zeroCheck() {
-  if (health < 0) {
-    health = 0
+  if (target.health < 0) {
+    target.health = 0;
   }
 }
+
+// let attilla = {
+//   health: 140,
+//   targetName: "Attilla",
+//   hits: 0,
+//   img: "Attilla.jpg",
+//   items: [],
+// };
+
+// let genghisKhan = {
+//   health: 130,
+//   targetName: "Genghis Khan",
+//   hits: 0,
+//   img: "GenghisKhan.PNG",
+//   items: [],
+// };
+
+// let vlad = {
+//   health: 100,
+//   targetName: "Vlad the Impaler",
+//   hits: 0,
+//   img: "Vlad.jpeg",
+//   items: [],
+// };
