6852ace3ac3fedf4b9846c8b82fe8ce227a82f81
Kyle Bennion <bilekennion@hotmail.com>
2020-08-20 14:47:35 -0600

    added picturs

--- game.js
+++ game.js
@@ -2,26 +2,82 @@ var health = 100
 var targetName = 'Ben'
 var hits = 0
 
-function hitCount() {
 
+let toons = [
+  {
+    health: 140,
+    targetName: "Attilla",
+    hits: 0,
+    img: 'Attilla.jpg'
+  },
+  {
+    health: 130,
+    targetName: "Genghis Khan",
+    hits: 0,
+    img:
+  },
+  {
+    health: 100,
+    targetName: "Vlad the Impaler",
+    hits: 0,
+  }
+]
+
+
+
+
+function hitCount() {
+  hits++
+
+}
+
+function displayHits(hits) {
+  let elem = document.getElementById('hits')
+  elem.innerHTML = hits.toString()
 }
 
 function slap() {
   health--
-  // console.log(health)
-  return healthDisplay(health)
+  hitCount()
+  zeroCheck()
+  healthDisplay(health)
+  displayHits(hits)
+
 }
+
 function healthDisplay(health) {
   let elem = document.getElementById('health')
-  elem.innerHTML = health.toString()
+  if (health == 0) {
+    health = 'Dead'
+    elem.innerHTML = health
+  }
+  else {
+    elem.innerHTML = health.toString()
+  }
 }
 
 function punch() {
   health -= 5
-  return healthDisplay(health)
+
+  hitCount()
+  zeroCheck()
+
+  healthDisplay(health)
+  displayHits(hits)
 }
 
 function kick() {
   health -= 10
-  return healthDisplay(health)
+
+  hitCount()
+  zeroCheck()
+
+  healthDisplay(health)
+  displayHits(hits)
+}
+
+function zeroCheck() {
+  if (health < 0) {
+    health = 0
+  }
 }
